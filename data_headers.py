e58b793eeb778f7a3ed130355d05f6bd68d674cb
robertgraefe <39485308+robertgraefe@users.noreply.github.com>
2020-11-03 13:42:27 +0100

    Restructure

--- data_headers.py
+++ data_headers.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
 
+print("hello world")
+
 DATASET_DIR = r"C:\Users\rober\Google Drive\Masterarbeit\Datensatz\2. Datasets Kaggle\\"
 # url = DATASET_DIR + "application_test.csv"
 url = DATASET_DIR + "bureau.csv"
-
 
 pd.options.display.max_rows = None
 pd.options.display.max_columns = None
