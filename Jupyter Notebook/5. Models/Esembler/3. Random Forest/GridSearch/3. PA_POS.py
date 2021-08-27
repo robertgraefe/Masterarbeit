2eece2e475e1920fae003c7a7f0b3d2d5aa608b9
robertgraefe <39485308+robertgraefe@users.noreply.github.com>
2021-08-27 14:04:16 +0200

    Update

--- Jupyter Notebook/5. Models/Esembler/3. Random Forest/GridSearch/3. PA_POS.py
+++ Jupyter Notebook/5. Models/Esembler/3. Random Forest/GridSearch/3. PA_POS.py
@@ -14,20 +14,20 @@ else:
     DATASET_DIR = path2
 
 app_train = pd.read_csv(DATASET_DIR / "4. FillNA" / "application.csv")
-bureau = pd.read_csv(DATASET_DIR / "4. FillNA" / "bureau.csv")
+pa_pos = pd.read_csv(DATASET_DIR / "4. FillNA" / "pa_pos.csv")
 
 app_train = app_train.set_index("SK_ID_CURR")
-bureau = bureau.set_index("SK_ID_CURR")
+pa_pos = pa_pos.set_index("SK_ID_CURR")
 
-MODEL_BUREAU = "3.2. Esembler_RandomForest_bureau.json"
+MODEL_PA_POS = "3.3. Esembler_RandomForest_pa_pos.json"
 
-with open(DATASET_DIR / "Models" / MODEL_BUREAU, 'r') as file:
-    model_bureau_data = json.load(file)
+with open(DATASET_DIR / "Models" / MODEL_PA_POS, 'r') as file:
+    model_pa_pos_data = json.load(file)
 
 # Random Forest
 TREE_PARAMS = {
-    "max_depth": [15],
-    "min_samples_leaf": [15,17,20],
+    "max_depth": [13,15,17],
+#    "min_samples_leaf": [15,17,20],
 #    "n_estimators": [800, 900, 1000],
     "random_state": [0],
     "n_jobs": [-1]
@@ -37,15 +37,15 @@ model = RandomForestClassifier()
 
 gridsearch = GridSearchCV(model, TREE_PARAMS, scoring='roc_auc', n_jobs=-1)
 
-x = bureau[model_bureau_data["keep"]]
-y = app_train.loc[bureau.index]["TARGET"]
+x = pa_pos[model_pa_pos_data["keep"]]
+y = app_train.loc[pa_pos.index]["TARGET"]
 
 gridsearch.fit(x,y)
 
 print(gridsearch.best_params_)
 print(gridsearch.best_score_)
 
-model_bureau_data["params"] = gridsearch.best_params_
+model_pa_pos_data["params"] = gridsearch.best_params_
 
-with open(DATASET_DIR / "Models" / MODEL_BUREAU, 'w') as file:
-    json.dump(model_bureau_data, file)
+with open(DATASET_DIR / "Models" / MODEL_PA_POS, 'w') as file:
+    json.dump(model_pa_pos_data, file)
